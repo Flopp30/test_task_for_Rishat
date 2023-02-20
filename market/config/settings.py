0a199cc48e131f9b5ec97abf27b8500225108e3e
master <pospeev.artem@icloud.com>
2023-02-20 22:33:35 +0400

    added keys for stripe

--- market/config/settings.py
+++ market/config/settings.py
@@ -118,3 +118,9 @@ else:
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Stripe setup
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
+STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET')
+
